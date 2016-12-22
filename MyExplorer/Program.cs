4c76ed40f34029d15679abc1ac7bb572295e75b7
Astralius <adrian.standowicz@gmail.com>
2016-12-22 16:38:33 +0100

    Files validation & simple enumeration
    Implemented additional features as per 'Krok 2' and 'Krok 3'.

--- MyExplorer/Program.cs
+++ MyExplorer/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace MyExplorer
 {
@@ -9,7 +10,40 @@ namespace MyExplorer
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 1) return;
+            string path = args.First();
+
+            // Walidacja dostępu: exists = false, gdy ścieżka jest niepoprawna -lub- katalog nie istnieje -lub- użytkownik nie ma prawa odczytu.
+            bool exists = Directory.Exists(path);
+
+            Console.WriteLine(exists ? "Katalog istnieje." : "Katalog nie istnieje");  
+            if(!exists) return;
+
+            // Domyślnym zachowaniem jest wyświetlenie samych plików. Zgodnie ze specyfikacją (Krok 3): 
+            // "- dokonała enumeracji wszystkich plików w folderze i wypisała ich nazwy na konsoli wraz ze ścieżką"
+            // Opcja: Jeżeli autor miał na myśli wszystkie pliki oraz foldery, można wywołać EnumerateContents(path, true).
+            // Opcja: Jeżeli autor miał na myśli wszystkie pliki z uwzględnieniem podfolderów i ich zawartości, można wywołać EnumerateContents(path, true, true).
+            EnumerateContents(path);                               
+        }
+
+        /// <summary>
+        /// Lists the contents of the directory from a given, valid path.
+        /// </summary>
+        /// <param name="path">Location (path) of the directory to enumerate.</param>
+        /// <param name="showDirectories">Should the listing include directories (folders)?</param>
+        /// <param name="includeSubfolders">Should the listing also include subdirectories and their contents?</param>
+        public static void EnumerateContents(string path, bool showDirectories = false, bool includeSubfolders = false)
+        {
+            IEnumerable<string> entries = (includeSubfolders)
+                ? Directory.EnumerateFileSystemEntries(path, "*", SearchOption.AllDirectories)
+                : Directory.EnumerateFileSystemEntries(path, "*", SearchOption.TopDirectoryOnly);
+
+            foreach (string entry in entries)
+            {
+                bool isDirectory = (File.GetAttributes(entry) & FileAttributes.Directory) == FileAttributes.Directory;
+                if (isDirectory && !showDirectories) continue;
+                Console.WriteLine(entry);
+            }
         }
     }
 }
