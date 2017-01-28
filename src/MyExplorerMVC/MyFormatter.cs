fb01b8c38b49917951c215a137072a73a0236a00
Astralius <adrian.standowicz@gmail.com>
2017-01-28 13:09:30 +0100

    Krok 2
    Implemented functionalities required by "Krok 2" specification.

--- src/MyExplorerMVC/MyFormatter.cs
+++ src/MyExplorerMVC/MyFormatter.cs
@@ -6,12 +6,14 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Explorer.Services;
 
 namespace MyExplorerMVC
 {
     public class MyFormatter
     {
         private const string TextHtmlUtf8 = "text/html; charset=utf-8";
+
         private readonly HtmlEncoder _htmlEncoder;
 
         public MyFormatter(HtmlEncoder encoder)
@@ -39,7 +41,7 @@ namespace MyExplorerMVC
 
             context.Response.ContentType = TextHtmlUtf8;
 
-            PathString requestPath = context.Request.PathBase;
+            PathString requestPath = context.Request.PathBase + context.Request.Path;
 
             var builder = new StringBuilder();
 
@@ -108,10 +110,129 @@ namespace MyExplorerMVC
             {
                 builder.AppendFormat(@"
                             <tr class=""file"">
-                                <td class=""name"">{0}</td>
+                                <td class=""name""><a href=""./{0}"">{0}</a></td>
                             </tr>",
                     HtmlEncode(file.Name));
             }
+
+            builder.Append(@"
+                        </tbody>
+                    </table>
+                    </section>
+                </body>
+                </html>");
+
+            string data = builder.ToString();
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            context.Response.ContentLength = bytes.Length;
+            return context.Response.Body.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        /// <summary>
+        /// Generates an HTML view for a single file.
+        /// </summary>
+        public virtual Task ListFileMetadataAsync(HttpContext context, MyFile contents)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (contents == null)
+            {
+                throw new ArgumentNullException(nameof(contents));
+            }
+
+            context.Response.ContentType = TextHtmlUtf8;
+
+            PathString requestPath = context.Request.PathBase + context.Request.Path;
+
+            var builder = new StringBuilder();
+
+            builder.AppendFormat(@"
+                <!DOCTYPE html>
+                <html lang=""{0}"">", CultureInfo.CurrentUICulture.TwoLetterISOLanguageName);
+
+            builder.AppendFormat(@"
+                <head>
+                    <title>{0} {1}</title>", "Informacje o pliku ", contents.Name);
+
+            builder.Append(@"
+                    <style>
+                        body {
+                            font-family: ""Segoe UI"", ""Segoe WP"", ""Helvetica Neue"", 'RobotoRegular', sans-serif;
+                            font-size: 14px;}
+                        header h1 {
+                            font-family: ""Segoe UI Light"", ""Helvetica Neue"", 'RobotoLight', ""Segoe UI"", ""Segoe WP"", sans-serif;
+                            font-size: 28px;
+                            font-weight: 100;
+                            margin-top: 5px;
+                            margin-bottom: 0px;}
+                        #index {
+                            border-collapse: separate; 
+                            border-spacing: 0; 
+                            margin: 0 0 20px; }
+                        #index th {
+                            vertical-align: bottom;
+                            padding: 10px 5px 5px 5px;
+                            font-weight: 400;
+                            color: #a0a0a0; }                            
+                        #index td { padding: 3px 10px; }
+                        #index th, #index td {
+                            border-right: none;
+                            border-bottom: none;
+                            border-left: none;
+                            border-top: none;
+                            box-sizing: border-box; }
+                        #index th.nameHeader, th.pathHeader { text-align:left; }
+                        #index th.createdHeader, th.modifiedHeader { text-align:right };
+                        #index td.created, td.modified { text-align:right; }
+                        a { color:#1ba1e2;text-decoration:none; }
+                        a:hover { color:#13709e;text-decoration:underline; }
+                    </style>
+                </head>
+                <body>
+                    <section id=""main"">");
+
+            builder.AppendFormat(@"
+                    <header><h1>{0} <a href=""/"">/</a>", "Plik ", requestPath);
+
+            string cumulativePath = "/";
+            foreach (var segment in requestPath.Value.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                cumulativePath = cumulativePath + segment + "/";
+                builder.AppendFormat(@"<a href=""{0}"">{1}/</a>",
+                    HtmlEncode(cumulativePath), HtmlEncode(segment));
+            }
+
+            builder.AppendFormat(CultureInfo.CurrentUICulture, @"
+                    </h1></header>
+                    <table id=""index"" summary=""{0}"">
+                        <thead>
+                            <tr>
+                                <th class=""nameHeader"" abbr=""{1}"">{1}</th>
+                                <th class=""pathHeader"" abbr=""{2}"">{2}</th>
+                                <th class=""createdHeader"" abbr=""{3}"">{3}</th>
+                                <th class=""modifiedHeader"" abbr=""{4}"">{4}</th>
+                            </tr>
+                        </thead>
+                        <tbody>",
+                    "Szczegółowe informacje o wybranym pliku.",
+                    "Nazwa",
+                    "Sciezka",
+                    "Data Utworzenia",
+                    "Data Modyfikacji");
+
+            builder.AppendFormat(@"
+                        <tr class=""file"">
+                            <td class=""name"">{0}</td>
+                            <td class=""path"">{1}</td>
+                            <td class=""created"">{2}</td>
+                            <td class=""modified"">{3}</td>
+                        </tr>",
+                HtmlEncode(contents.Name),
+                HtmlEncode(contents.FullPath),
+                HtmlEncode(contents.DateCreated.ToString(CultureInfo.CurrentCulture)),
+                HtmlEncode(contents.DateModified.ToString(CultureInfo.CurrentCulture)));
 
             builder.Append(@"
                         </tbody>
--- src/MyExplorerMVC/Startup.cs
+++ src/MyExplorerMVC/Startup.cs
@@ -3,7 +3,6 @@ using System.Text.Encodings.Web;
 using Explorer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +15,8 @@ namespace MyExplorerMVC
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
+            services.AddDirectoryBrowser();
             formatter = new MyFormatter(HtmlEncoder.Default);
         }
 
@@ -26,8 +27,20 @@ namespace MyExplorerMVC
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(context => formatter.ListFilesAsync(
-                    context, FileService.GetFiles(env.ContentRootPath, true)));
+            var routeBuilder = new RouteBuilder(app);
+
+            // Default behaviour: display a list of all files in the project web root
+            routeBuilder.MapGet("",
+                context => formatter.ListFilesAsync(
+                    context, FileService.GetFiles(env.WebRootPath, true)));
+
+            // Display file based on its typed name (or clicked link)
+            routeBuilder.MapGet("{fileName}",
+                context => formatter.ListFileMetadataAsync(
+                                context, FileService.GetFileInfo(
+                                    env.WebRootPath + Path.DirectorySeparatorChar + context.GetRouteValue("fileName").ToString())));
+
+            app.UseRouter(routeBuilder.Build());
         }
     }
 }
